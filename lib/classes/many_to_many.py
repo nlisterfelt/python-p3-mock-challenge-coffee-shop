f9ba397dc752ad148b4d54ba5790f72e3515659e
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-10-18 22:04:53 -0500

    coffee customer order

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,98 @@
 class Coffee:
     def __init__(self, name):
         self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name)>2 and not hasattr(self, "name"):
+            self._name = name
         
     def orders(self):
-        pass
+        return [order for order in Order.all if self == order.coffee]
     
     def customers(self):
-        pass
+        unique_customers = []
+        for order in Order.all:
+            if order.coffee == self and order.customer not in unique_customers:
+                unique_customers.append(order.customer)
+        return unique_customers
     
     def num_orders(self):
-        pass
+        count = 0
+        for order in Order.all:
+            if order.coffee == self:
+                count += 1
+        return count
     
     def average_price(self):
-        pass
+        total_prices = [order.price for order in Order.all if order.coffee == self]
+        sum_prices = sum(total_prices)
+        num_prices = len(total_prices)
+        return sum_prices/num_prices
 
 class Customer:
     def __init__(self, name):
         self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name)>0 and len(name)<16:
+            self._name = name
         
     def orders(self):
-        pass
+        return [order for order in Order.all if order.customer == self]
     
     def coffees(self):
-        pass
+        unique_coffees = []
+        for order in Order.all:
+            if order.customer == self and order.coffee not in unique_coffees:
+                unique_coffees.append(order.coffee)
+        return unique_coffees
     
     def create_order(self, coffee, price):
-        pass
+        if isinstance(coffee, Coffee):
+            return Order(self, coffee, price)
     
 class Order:
+    all = []
+
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+        Order.all.append(self)
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, price):
+        if isinstance(price, float) and price >= 1.0 and price <= 10.0 and not hasattr(self, "price"):
+            self._price = price
+
+    @property
+    def customer(self):
+        return self._customer
+
+    @customer.setter
+    def customer(self, customer):
+        if isinstance(customer, Customer):
+            self._customer = customer
+
+    @property
+    def coffee(self):
+        return self._coffee
+
+    @coffee.setter
+    def coffee(self, coffee):
+        if isinstance(coffee, Coffee):
+            self._coffee = coffee
